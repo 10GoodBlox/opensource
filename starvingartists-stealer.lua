dff282f35597eb55383533cfda82a0051cba2e87
DogePlayz-Dev <58870118+DogePlayz-Dev@users.noreply.github.com>
2022-04-02 17:24:54 -0700

    Create starvingartists-stealer.lua

---
+++ starvingartists-stealer.lua
@@ -0,0 +1,9 @@
+if game.PlaceId ~= then return end
+local canvas = game:GetService("Workspace").Plots[targetUser].Easels:FindFirstChild(targetCanvas)
+
+for _,px in pairs(canvas.Canvas.SurfaceGui.Grid:GetChildren()) do
+    local ypx = game:GetService("Players").LocalPlayer.PlayerGui.MainGui.PaintFrame.GridHolder.Grid[px.Name]
+    if ypx:IsA("TextButton") then
+        ypx.BackgroundColor = px.BackgroundColor
+    end
+end
